3d87f235144937548603129ca38d1afceeb6d652
george2 <involver7@gmail.com>
2022-03-19 04:53:13 -0700

    Start dynamic programming problem

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -14,16 +14,32 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     """
     Find number of eggs to bring back, using the smallest number of eggs. Assumes there is
     an infinite supply of eggs of each weight, and there is always a egg of value 1.
-    
+
     Parameters:
     egg_weights - tuple of integers, available egg weights sorted from smallest to largest value (1 = d1 < d2 < ... < dk)
     target_weight - int, amount of weight we want to find eggs to fit
     memo - dictionary, OPTIONAL parameter for memoization (you may not need to use this parameter depending on your implementation)
-    
+
     Returns: int, smallest number of eggs needed to make target weight
     """
-    # TODO: Your code here
-    pass
+    total = 0
+
+    # base case
+    if len(egg_weights) == 1:
+        result = egg_weights[-1] // target_weight
+        total += result
+        target_weight = target_weight % egg_weights[-1]
+    # add to memo
+        memo[egg_weights[-1]] = result
+
+    # base case + 1
+    elif len(egg_weights) == 2
+
+    else:
+        dp_make_weight(egg_weights, target_weight)
+
+    # progress to base case
+
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
