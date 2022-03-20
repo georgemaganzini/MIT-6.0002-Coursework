1b0f8c734b5069f80046cac349baea690f176a8c
george2 <involver7@gmail.com>
2022-03-20 11:27:44 -0700

    Complete ps1B

--- ps1/ps1b.py
+++ ps1/ps1b.py
@@ -9,6 +9,7 @@
 # Part B: Golden Eggs
 #================================
 
+
 # Problem 1
 def dp_make_weight(egg_weights, target_weight, memo = {}):
     """
@@ -22,31 +23,63 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
 
     Returns: int, smallest number of eggs needed to make target weight
     """
-    total = 0
+
+
+
+    least_taken = float("inf")
 
     # base case
-    if len(egg_weights) == 1:
-        result = egg_weights[-1] // target_weight
-        total += result
-        target_weight = target_weight % egg_weights[-1]
-    # add to memo
-        memo[egg_weights[-1]] = result
+    if target_weight == 0:
+        return 0
 
-    # base case + 1
-    elif len(egg_weights) == 2
+    # check if we have already solved for this target_weight
+    elif target_weight in memo:
+        return memo[target_weight]
 
-    else:
-        dp_make_weight(egg_weights, target_weight)
+    # recursively check all tree paths while storing the most efficient (least # of eggs)
+    elif target_weight > 0:
+        for weight in egg_weights:
+            sub_result = dp_make_weight(egg_weights, target_weight - weight)
+            least_taken = min(least_taken, sub_result)
 
-    # progress to base case
+    memo[target_weight] = least_taken + 1
+    return least_taken + 1
+
+
+# with lru_cache library
+
+# from functools import lru_cache
+# @lru_cache
+
+# def dp_make_weight(egg_weights, target_weight, memo = {}):
+
+#     least_taken = float("inf")
+
+#     if target_weight == 0:
+#         return 0
+#     elif target_weight > 0:
+#         for weight in egg_weights:
+#             sub_result = dp_make_weight(egg_weights, target_weight - weight)
+#             least_taken = min(least_taken, sub_result)
+
+#     return least_taken + 1
 
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
 if __name__ == '__main__':
-    egg_weights = (1, 5, 10, 25)
-    n = 99
-    print("Egg weights = (1, 5, 10, 25)")
-    print("n = 99")
-    print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
-    print("Actual output:", dp_make_weight(egg_weights, n))
-    print()
+    # egg_weights = (1, 5, 10, 25)
+    # n = 99
+    # print("Egg weights = (1, 5, 10, 25)")
+    # print("n = 99")
+    # print("Expected ouput: 9 (3 * 25 + 2 * 10 + 4 * 1 = 99)")
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    # print()
+
+    # egg_weights = (1, 30, 60, 75) # egg_weight distrubution that would not be solved optimally by greedy algorithm
+    # n = 90
+    # print("Egg weights = (1, 30, 60, 75")
+    # print("n = 90")
+    # print("Expected ouput: 2 (0 * 75 + 1 * 60 + 1 * 30 + 0 * 1 = 90)")
+
+    # print("Actual output:", dp_make_weight(egg_weights, n))
+    # print()
